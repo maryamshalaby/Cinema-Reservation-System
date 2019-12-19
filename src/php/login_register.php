3c3d3a47399cf430cf263e5c0ed006faae29d793
Maryam's Laptop <maryam_shalaby@yahoo.com>
2019-12-19 20:26:19 +0200

    added login/register script

---
+++ src/php/login_register.php
@@ -0,0 +1,57 @@
+<?php
+$servername = "localhost";
+$DBcred = "root";
+$DBpass = "";
+$DBname ="cinema_reservation";
+
+
+
+// Create connection
+$conn = new mysqli($servername, $DBcred, $DBpass,$DBname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully <br>";
+
+$op= isset($_POST['op']) ? $_POST['op'] : '';
+
+if($op=="login"){
+
+    $username= isset($_POST['username']) ? $_POST['username'] : '';
+    $password= isset($_POST['pass']) ? $_POST['pass'] : '';
+
+    $sql='SELECT `type` FROM `users` WHERE `username`="'.$username.'" and `password`="'.$password.'";';
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+           echo "type:" . $row["type"];
+        }
+     } else {
+        echo "Error";
+     }
+   
+
+}else{
+    $username= isset($_POST['username']) ? $_POST['username'] : '';
+    $password= isset($_POST['pass']) ? $_POST['pass'] : '';
+    $first_name= isset($_POST['first_name']) ? $_POST['first_name'] : '';
+    $last_name= isset($_POST['last_name']) ? $_POST['last_name'] : '';
+    $birth_date= isset($_POST['birth_date']) ? $_POST['birth_date'] : '';
+    $email= isset($_POST['email']) ? $_POST['email'] : 'dummy@';
+    $type= isset($_POST['type']) ? $_POST['type'] : '';
+
+
+    $sql='INSERT INTO `users`(`username`, `password`, `first_name`, `last_name`, `birth_date`, `email`, `type`) VALUES ("'.$username.'", "'.$password.'", "'.$first_name.'", "'.$last_name.'", "'.$birth_date.'", "'.$email.'", "'.$type.'")';
+   
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+     } else {
+        echo "Error: " . mysqli_error($conn);
+     }
+}
+
+mysqli_close($conn);
+?>
